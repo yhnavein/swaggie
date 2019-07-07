37badd23122e40a5e3634e0df59eafd2c6c844f2
Piotrek Dąbrowski <floubadour@gmail.com>
2019-07-07 19:32:04 +0200

    fix: test config (WIP)

--- test/spec/spec.spec.ts
+++ test/spec/spec.spec.ts
@@ -4,21 +4,21 @@ import expect from 'expect';
 describe('spec', () => {
   it('should resolve a spec from url', async () => {
     const spec = await resolveSpec('http://petstore.swagger.io/v2/swagger.json');
-    expect(spec).toExist();
+    expect(spec).toBeDefined();
     expect(spec.host).toBe('petstore.swagger.io');
     expect(spec.basePath).toBe('/v2');
-    expect(spec.securityDefinitions).toExist();
-    expect(spec.definitions).toExist();
-    expect(spec.paths).toExist();
+    expect(spec.securityDefinitions).toBeDefined();
+    expect(spec.definitions).toBeDefined();
+    expect(spec.paths).toBeDefined();
   });
 
   it('should resolve a spec from local file', async () => {
     const path = `${__dirname}/../petstore.yml`;
     const spec = await resolveSpec(path);
-    expect(spec).toExist();
+    expect(spec).toBeDefined();
     expect(spec.host).toBe('petstore.swagger.io');
     expect(spec.basePath).toBe('/v1');
-    expect(spec.definitions).toExist();
-    expect(spec.paths).toExist();
+    expect(spec.definitions).toBeDefined();
+    expect(spec.paths).toBeDefined();
   });
 });
