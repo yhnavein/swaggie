167ab11bf4edd6beea18681e5c42450a9743e0fc
Piotrek Dąbrowski <floubadour@gmail.com>
2020-03-16 23:11:35 +0100

    feat: add support for serialize complex query params

---
+++ src/gen/js/serializeQueryParam.spec.ts
@@ -0,0 +1,63 @@
+/** This file tests a code that will be generated and is hardcoded into the templates */
+
+describe('serializeQueryParam', () => {
+  [
+    { input: '', expected: '' },
+    { input: null, expected: '' },
+    { input: undefined, expected: '' },
+    { input: 123, expected: '123' },
+    { input: 'test string', expected: 'test%20string' },
+    { input: {}, expected: '' },
+    { input: { a: 1, b: 'test' }, expected: 'a=1&b=test' },
+    { input: { a: 1, b: [1, 2, 3, 'test'] }, expected: 'a=1&b=1%2C2%2C3%2Ctest' },
+    { input: [1, 2, 3], expected: '1%2C2%2C3' },
+  ].forEach((el) => {
+    it(`should handle ${JSON.stringify(el.input)}`, () => {
+      const res = serializeQueryParam(el.input);
+
+      expect(res).toBe(el.expected);
+    });
+  });
+
+  function serializeQueryParam(obj: any) {
+    if (obj === null || obj === undefined) return '';
+    if (typeof obj !== 'object' || Array.isArray(obj)) return encodeURIComponent(obj);
+    return Object.keys(obj)
+      .reduce((a, b) => a.push(encodeURIComponent(b) + '=' + encodeURIComponent(obj[b])) && a, [])
+      .join('&');
+  }
+});
+
+/** This is different, because we don't need to encode parameters for axios as axios is doing it on its own */
+describe('serializeQueryParam / axios', () => {
+  [
+    { input: '', expected: '' },
+    { input: null, expected: '' },
+    { input: undefined, expected: '' },
+    { input: 123, expected: 123 },
+    { input: 'test string', expected: 'test string' },
+    { input: {}, expected: '' },
+    { input: { a: 1, b: 'test' }, expected: 'a=1&b=test' },
+    { input: { a: 1, b: [1, 2, 3, 'test'] }, expected: 'a=1&b=1,2,3,test' },
+  ].forEach((el) => {
+    it(`should handle ${JSON.stringify(el.input)}`, () => {
+      const res = serializeQueryParam(el.input);
+
+      expect(res).toBe(el.expected);
+    });
+  });
+
+  it(`should handle array`, () => {
+    const res = serializeQueryParam([1, 2, 3]);
+
+    expect(res.toString()).toBe([1, 2, 3].toString());
+  });
+
+  function serializeQueryParam(obj: any) {
+    if (obj === null || obj === undefined) return '';
+    if (typeof obj !== 'object' || Array.isArray(obj)) return obj;
+    return Object.keys(obj)
+      .reduce((a, b) => a.push(b + '=' + obj[b]) && a, [])
+      .join('&');
+  }
+});
